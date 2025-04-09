3c4e75359e884f16ff0a16f1d751458bb324fee3
Nat Oliven <oliven@umich.edu>
2025-04-09 10:17:35 -0400

    maybe solved the import issue? now renaming repo with underscore

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package_name',
+    name='phi-generation',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'langchain',
-        'ipykernel',
-        # Other dependencies...
+        'langchain-community',  # Replace langchain with langchain-community
+        'langchain-openai',     # New package
+        'openai',               # New package
+        'chromadb',             # New package
+        'python-dotenv',        # New package
+        'pytest',               # New package
+        'pandas',               # New package
+        'ipykernel',            # Already included
+        'tabulate',             # New package
     ],
     # ... other metadata
 )
